85bc430b2318dfa7adeb5cba5050fe01123597dd
Rxluizin <141074195+Rxluizin@users.noreply.github.com>
2023-09-20 08:56:31 -0400

    Update radio.js

--- radio.js
+++ radio.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const playlist = document.getElementById("playlist").getElementsByTagName("li");
+    const prevButton = document.getElementById("prev-button");
+    const nextButton = document.getElementById("next-button");
+    let currentTrack = 0;
+
+    function playTrack(trackIndex) {
+        for (let i = 0; i < playlist.length; i++) {
+            playlist[i].querySelector("audio").pause();
+            playlist[i].querySelector("audio").currentTime = 0;
+        }
+        playlist[currentTrack].querySelector("audio").play();
+    }
+
+    playTrack(currentTrack);
+
+    prevButton.addEventListener("click", function () {
+        currentTrack = (currentTrack - 1 + playlist.length) % playlist.length;
+        playTrack(currentTrack);
+    });
+
+    nextButton.addEventListener("click", function () {
+        currentTrack = (currentTrack + 1) % playlist.length;
+        playTrack(currentTrack);
+    });
+});
 
